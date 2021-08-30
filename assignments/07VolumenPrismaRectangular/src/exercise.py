63b30ee14f7c69b56651cbdbb9e956f47d0436bc
Zitlalliml <89087259+Zitlalliml@users.noreply.github.com>
2021-08-30 17:07:25 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,19 +1,17 @@
 
 def main():
-    #escribe tu código abajo de esta línea
-    def main():
-        b=float(input('Dame la base: '))
-        h=float(input('Dame la altura: '))
-        A= b*h
+    b=float(input('Dame la base: '))
+    h=float(input('Dame la altura: '))
+    A= b*h
 
-        volumen(A)
-        return(A)
+    volumen(A)
+    return(A)
 
-    def volumen (t):
-        p=float(input('Dame la profundidad: '))
-        vol= t * p
+def volumen (t):
+     p=float(input('Dame la profundidad: '))
+     vol= t * p
 
-        print('El volumen del prisma es:' + str(vol))
+    print('El volumen del prisma es:' + str(vol))
     pass
 
 if __name__=='__main__':
