0c3c88dcf21c604304304eebf7738ee5035eb009
Zitlalliml <89087259+Zitlalliml@users.noreply.github.com>
2021-08-30 19:16:05 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,13 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    año=int(input())
+    if (año % 4) == 0 and (año % 100) != 0 or (año% 400) == 0:
+        bin= True
+    else:
+        bin= False
+  
+    print (str(bin))
     pass
 
 if __name__=='__main__':
